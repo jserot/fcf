f170de3c72f319266e4d4e929d977281bf8b613e
jserot <jocelyn.serot@uca.fr>
2022-05-29 19:46:26 +0200

    Fixed bug in VHDL heap allocation fn

--- src/bin/vhdl_heap.ml
+++ src/bin/vhdl_heap.ml
@@ -24,7 +24,14 @@ exception Heap_full
         
 let make sz = Array.make sz Empty
 
-let rec alloc variants heap h_ptr (e:Syntax.expr) =
+let string_of_value = function   (* for debug only *)
+| Imm (Int v) -> "Imm<" ^ string_of_int v ^ ">"
+| Imm (Bool v) -> "Imm<" ^ string_of_bool v ^ ">"
+| Imm (Float v) -> "Imm<" ^ string_of_float v ^ ">"
+| Ptr p -> "Ptr<" ^ string_of_int p ^ ">"
+
+let rec alloc ?(depth=2) variants heap h_ptr (e:Syntax.expr) =
+  (* let tab sz = String.make sz '*' in (\* for debug only *\) *)
   let open Syntax in
   match e.e_desc,vhdl_type_of (e.e_typ) with
   | EInt i, Unsigned _
@@ -38,19 +45,29 @@ let rec alloc variants heap h_ptr (e:Syntax.expr) =
      Imm (Int vc.vc_tag), h_ptr  (* no alloc: nullary ctors are represented by their tag *)
   | ECon1 (c,args), Variant vd -> 
      (try
-       let args' = begin match args.Syntax.e_desc with Syntax.ETuple es -> es | _ -> [args] end in
+       (* Printf.printf "%s Vhdl_heap.alloc(%s, h_ptr=%d)\n" (tab depth) (Syntax.string_of_expr e) h_ptr; *)
        let vd' = lookup_variant_desc vd.vd_name variants in
        let vc = lookup_variant_ctor c vd'.vd_ctors in 
-       heap.(h_ptr) <- Header (vc.vc_tag, vc.vc_arity);
-       let h_ptr' = 
-         Misc.list_fold_lefti  
-           (fun i h_ptr' arg ->
-             let v, h_ptr'' = alloc variants heap (h_ptr+vc.vc_arity+1) arg in 
-             heap.(h_ptr+i+1) <- Value v;
-             h_ptr'')
+       let args' = begin match args.Syntax.e_desc with Syntax.ETuple es -> es | _ -> [args] end in
+       (* Allocate arguments first ... *)
+       let v_args, h_ptr' = 
+         Misc.list_map_fold  
+           (fun h_ptr arg ->
+             let v, h_ptr' = alloc ~depth:(depth+2) variants heap h_ptr arg in 
+             v, h_ptr')
            h_ptr
            args' in
-       Ptr h_ptr, h_ptr'
+       (* Printf.printf "%s values=[%s] h_ptr'=%d\n" (tab depth) (Misc.string_of_list string_of_value "," v_args) h_ptr'; *)
+       (* ... then the block header *)
+       heap.(h_ptr') <- Header (vc.vc_tag, vc.vc_arity);
+       (* Printf.printf "%s heap[%d]=<%d,%d>\n" (tab depth) h_ptr' vc.vc_tag vc.vc_arity; *)
+       List.iteri
+         (fun i v ->
+           heap.(h_ptr'+i+1) <- Value v
+           (* Printf.printf "%s heap[%d]=%s\n" (tab depth) (h_ptr'+i+1) (string_of_value v) *)
+           )
+         v_args;
+       Ptr h_ptr', h_ptr'+vc.vc_arity+1
      with
        Invalid_argument _ -> raise Heap_full)
   | _, _ -> failwith ("Heap.alloc: illegal value: " ^ Syntax.string_of_expr e ^ ": " ^ Types.string_of_type e.e_typ)
